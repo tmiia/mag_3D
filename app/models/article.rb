9cd238e2e78ee12d7fe76da8d1b182bf9591c620
presqueasmr <auriane1103@gmail.com>
2023-10-20 15:42:01 +0200

    added Article table

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :lead, presence: true, length: { maximum: 100 }
+    validates :text_content, presence: true
+    ## To delete when long_format / isDraft in no longer a checkbox 
+    validates :is_longformat, acceptance: true
+    validates :is_draft, acceptance: true
+end
