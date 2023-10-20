711366f027c5af102bf968a863c63db2d7846894
Olivia206 <olivia.simonet206@gmail.com>
2023-10-20 17:38:07 +0200

    fix: changed article model params

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :lead, presence: true, length: { maximum: 300 }
     validates :text_content, presence: true
-    ## To delete when long_format / isDraft in no longer a checkbox 
-    validates :is_longformat, acceptance: true
-    validates :is_draft, acceptance: true
+    ## To delete when long_format / isDraft in no longer a checkbox
+    
+    validates :is_longformat, inclusion: { in: [true, false] }, allow_blank: true
+    validates :is_draft, inclusion: { in: [true, false] }, allow_blank: true
 end
