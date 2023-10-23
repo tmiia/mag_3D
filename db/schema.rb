f22c1291cf6bb872e94c50e35be792f413b0565d
Olivia206 <olivia.simonet206@gmail.com>
2023-10-23 12:01:10 +0200

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_150744) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_152816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_150744) do
     t.string "pseudonym"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "article_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "articles", force: :cascade do |t|
@@ -59,6 +64,28 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_150744) do
     t.string "title"
     t.text "lead"
     t.text "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "polls", force: :cascade do |t|
+    t.text "question"
+    t.string "label_1"
+    t.integer "count_1"
+    t.string "label_2"
+    t.integer "count_2"
+    t.string "label_3"
+    t.integer "count_3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
