5199e838a443f2c07e49db1a0a3011273245dc42
Nehemia Telusma <telusma.nehemia@gmail.com>
2023-10-24 17:29:37 +0200

    feat: display actus & 360 by day

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_10_24_134833) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_133711) do
->>>>>>> bb021c9dd8d753dbe9f104cfd885ae7119e8ea06
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
