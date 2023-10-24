6a0c3abfa872f27ef3602e2f7f43dd400590bc99
presqueasmr <auriane1103@gmail.com>
2023-10-24 15:23:26 +0200

    validated relation poll-debate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_122913) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_131023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_122913) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "lead"
+    t.bigint "debate_id", null: false
+    t.index ["debate_id"], name: "index_polls_on_debate_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -109,4 +111,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_122913) do
   add_foreign_key "debates", "categories"
   add_foreign_key "favoris", "articles"
   add_foreign_key "favoris", "users"
+  add_foreign_key "polls", "debates"
 end
